cfff7ec6a6f42057166b10ab42de28d7db2b1574
kester <you@domain.com>
2022-04-25 01:21:26 +0800

    2nd commit

--- app.js
+++ app.js
@@ -138,6 +138,11 @@ app.get("/:typeOfList", (req, res) => {
   })
 });
 
-app.listen(process.env.POST || 3000, function() {
-  console.log("Server started on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+ 
+app.listen(port, function() {
+  console.log("Server started succesfully");
+});  
